e7b9dfc1a93ef6321c33ee61feda6867dfcf5cb2
Lucio Fernandes <luciofernandes@gmail.com>
2022-09-14 01:09:59 -0300

    Concluindo desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456", "Modelo 1", "11111111", 512);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone ipone = new Iphone("123456", "Modelo 1", "11111111", 512);
+ipone.ReceberLigacao();
+ipone.InstalarAplicativo("Facebook");
